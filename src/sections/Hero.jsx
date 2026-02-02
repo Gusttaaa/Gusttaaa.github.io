cfebe9bd93f0b5b4d2120199f4b05d99f810ca68
Gustavo Belmiro <gusttaafps@gmail.com>
2026-02-02 02:09:56 -0300

    button download

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -72,16 +72,20 @@ export const Hero = () => {
                             Hi, I'm Gusta. I'm a FullStack Developer who loves turning ideas into reality using React and Next.js. 
                             My experience ranges from developing automation tools to building immersive digital worlds, 
                             ensuring every project I touch is both technically robust and engaging.
-                        </p>
+                        </p> 
                     </div>
 
                     {/* CTAs */}
 
                     <div className="flex flex-wrap gap-4 animate-fade-in animation-delay-300">
-                        <Button size="lg">Contact Me<ArrowRight className="w-5 h-5"/></Button>
+                        <a href="#contact">
+                            <Button size="lg">
+                                Contact Me <ArrowRight className="w-5 h-5"/>
+                            </Button>
+                        </a>
                         <AnimatedBorderButton>
                             <Download className="w-5 h-5"/>
-                            Download CV
+                            <a href="/CV-Gustavo.pdf" download>Download CV</a>
                         </AnimatedBorderButton>
                     </div>
 
