748a93cdccccdfd396e8d71d98cc559b02942e03
Gustavo Belmiro <gusttaafps@gmail.com>
2026-02-02 01:07:24 -0300

    test new render react

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react({babel: {plugins: ['babel-plugin-react-compiler'],},}), tailwindcss()],
 
   resolve: {
     alias: {
